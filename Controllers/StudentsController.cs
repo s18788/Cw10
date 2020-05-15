4b62687692bf3667c74b408d568a60bd72708452
s18788 <61691140+s18788@users.noreply.github.com>
2020-05-16 01:53:17 +0200

    Yikes

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Cw10.Models;
+using System;
 
 namespace Cw10.Controllers
 {
@@ -25,67 +26,118 @@ namespace Cw10.Controllers
         }
 
         [HttpPost]
-        public IActionResult ModifyStudent([Bind("IndexNumber,FirstName,LastName,BirthDate,IdEnrollment")] Student student)
+        public IActionResult ModifyStudent(string IdNumber, string FirstName, string LastName, DateTime birthDate, int IdEnrollment)
         {
-
-            Student exists = _context.Find<Student>(student.IndexNumber);
-            if (exists != null)
-            {
-                _context.Update<Student>(student);
-                _context.SaveChanges();
-                return Ok("zmodyfikowano studenta");
-            }
-
+            /*   Student student = {
+                   IndexNumber = IdNumber,
+                   FirstName
+               };
+               Student exists = _context.Find<Student>(student.IndexNumber);
+               if (exists != null)
+               {
+                   _context.Update<Student>(student);
+                   _context.SaveChanges();
+                   return Ok("zmodyfikowano studenta");
+               }
+   */
             return NotFound();
         }
 
         [HttpDelete]
-        public IActionResult DeleteStudent([Bind("IndexNumber,FirstName,LastName,BirthDate,IdEnrollment")] Student student)
+        public IActionResult DeleteStudent(string IndexNumber)
         {
 
-            Student exists = _context.Find<Student>(student.IndexNumber);
-            if (exists != null)
+            if (IndexNumber != null)
             {
-                _context.Remove<Student>(student);
-                _context.SaveChanges();
-                return Ok("usunięto studenta");
+                Student exists = _context.Find<Student>(IndexNumber);
+                if (exists != null)
+                {
+                    _context.Remove<Student>(exists);
+                    _context.SaveChanges();
+                    return Ok("usunięto studenta");
+                }
             }
 
             return NotFound();
         }
 
-        [Route("api/enrollstudent")]
+        [Route("api/enrollments")]
         [HttpPost]
-        public IActionResult EnrollStudent(Student student, string studies)
+        public IActionResult EnrollStudent(string id, string fname, string name, DateTime bday, string studiesName)
         {
 
-            /*   if (student.IndexNumber == null
-                     || string.IsNullOrEmpty(student.FirstName)
-                     || string.IsNullOrEmpty(student.LastName)
-                     || string.IsNullOrEmpty(studies))
-               {
-                   return BadRequest();
-               }
+            if (id == null || string.IsNullOrEmpty(fname) || string.IsNullOrEmpty(name) || string.IsNullOrEmpty(studiesName))
+            {
+                return BadRequest();
+            }
 
-               if (_context.Find<Student>(student) != null)
-               {
-                   if (_context.Find<Studies>(studies)) {
+            Student student = _context.Find<Student>(id);
+
+            if (student != null)
+            {
+                return NotFound("Student już istnieje");
+            }
 
 
-                       _context.Add<Student>(student);
-                       _context.Add<Enrollment>(student.IdEnrollment);
-                       _context.SaveChanges();
-                   }
-               }*/
 
-            return NotFound();
+            var studies = _context.Studies.Where(s => s.Name.Equals(studiesName));
+
+            var idStudy = studies.Select(x => x.IdStudy);
+
+            if (studies == null)
+            {
+                return NotFound("nie ma takiego kierunku");
+            }
+
+
+            var maxEnrollment = _context.Enrollment.Max(x => x.IdEnrollment);
+            Enrollment enrollment = new Enrollment
+            {
+                IdEnrollment = maxEnrollment + 1,
+                Semester = 1,
+                IdStudy = 1,
+                StartDate = new DateTime(2020, 10, 1)
+            };
+
+            student = new Student
+            {
+                IndexNumber = id,
+                FirstName = fname,
+                LastName = name,
+                BirthDate = bday,
+                IdEnrollment = maxEnrollment + 1
+
+            };
+
+            _context.Add<Enrollment>(enrollment);
+            _context.Add<Student>(student);
+            _context.SaveChanges();
+
+            return Ok("zrekrutowano studenta");
         }
 
-        [Route("api/promotestudents")]
+        [Route("api/enrollments/promotions")]
         [HttpPost]
-        public IActionResult PromoteStudents()
+        public IActionResult PromoteStudents(string studies, int semester)
         {
-            return NotFound();
+
+            var res = _context.Studies.Where(x => x.Name.Equals(studies));
+
+            if (res == null)
+            {
+                return NotFound("nie ma takiego kierunku");
+            }
+
+            var idStudy = res.Select(x => x.IdStudy);
+
+            var toupdate = _context.Enrollment.Where(x => x.Semester == semester).Where(x => x.IdStudy == 2).ToList();
+
+            foreach (var e in toupdate)
+            {
+                e.Semester += 1;
+                _context.Enrollment.Update(e);
+            }
+            return Ok("Wypromowano studentów");
         }
 
 
